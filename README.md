9948df05e67621f0cc7c701bd5e8cbba66d8c5af
TakamiChie@Visnu <chie@onpu-tamago.net>
2019-01-11 13:26:54 +0900

    初回コミット

---
+++ README.md
@@ -0,0 +1,14 @@
+# SBCamp.PHP-App-Sources
+
+SBCamp. あなたのWebサイトに動きをつけよう！Javascript講座で使用しているプログラムのソースコードです。
+
+## 使用している技術・メソッド
+
+* alert()
+* console.log()
+* document.querySelector()
+* document.createElement()
+* document.addEventListener()
+  * DOMContentLoaded
+  * form#submit
+* IndexedDB
---
+++ index.js
@@ -0,0 +1,63 @@
+const DBNAME = 'sampleDB';
+const STORENAME = 'post';
+function addpost(post){
+  let viewarea = document.querySelector("#viewarea");
+  let name = document.createElement("dt");
+  let content = document.createElement("dd");
+  let time = document.createElement("dd");
+  name.textContent = post.name;
+  content.textContent = post.content;
+  time.textContent = post.time.getFullYear() + "年" +
+    (post.time.getMonth() + 1) + "月" +
+    post.time.getDate() + "日" +
+    post.time.getHours() + "時" +
+    post.time.getMinutes() + "分" +
+    post.time.getSeconds() + "秒";
+  viewarea.appendChild(name);
+  viewarea.appendChild(content);
+  viewarea.appendChild(time);
+}
+var db;
+document.addEventListener("DOMContentLoaded", (e) => {
+  document.querySelector("#postform").addEventListener("submit", (e) => {
+    let post = {
+      name: document.querySelector("#name").value,
+      content: document.querySelector("#content").value,
+      time: new Date()
+    }
+    // DB書き込み
+    let transaction = db.transaction(STORENAME, 'readwrite');
+    let os = transaction.objectStore(STORENAME);
+    let request = os.add(post);
+    request.onsuccess = () => {
+      console.log("db add success");
+    }
+
+    alert("投稿しました。");
+    document.querySelector("#name").value = "";
+    document.querySelector("#content").value = "";
+    e.preventDefault();
+    addpost(post);
+  });
+  // DB作成
+  let openReq  = indexedDB.open(DBNAME, 1);
+  openReq.onupgradeneeded = (e) => {
+    let dbsetup = e.target.result;
+    dbsetup.createObjectStore(STORENAME, { keyPath: "id", autoIncrement: true});
+    console.log("db created");
+  }
+  openReq.onsuccess = (e) => {
+    db = e.target.result;
+    let transaction = db.transaction(STORENAME, "readonly");
+    let os = transaction.objectStore(STORENAME);
+    document.querySelector("#submit_button").disabled = false;
+    os.openCursor().onsuccess = (e) => {
+      let c = e.target.result;
+      if(c){
+        console.log("loaded " + c.key);
+        addpost(c.value);
+        c.continue();
+      }
+    }
+  }
+});
